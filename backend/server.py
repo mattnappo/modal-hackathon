dd93e8d74009e695f00445842a830e9da8827d6f
Neil Yeung <nyeung@MacBook-Pro-7.local>
2023-08-26 16:08:52 -0400

    got the video transcription endpoint working

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,22 @@
 from typing import Union
 
 from fastapi import FastAPI
+import json
 import uvicorn
-
+from insights.transcript import transcribe_video
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/transcribe/")
+def transcribe_url(url: str):
+    return json.dumps(transcribe_video(url))
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
