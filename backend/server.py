f8e19a0e21081460390eddb0c921ed4ce6696b9f
LisaSamWang <sam_wang_lisa1121@berkeley.edu>
2023-08-26 19:12:24 -0400

    backend

--- backend/server.py
+++ backend/server.py
@@ -3,18 +3,22 @@ from typing import Union
 from fastapi import FastAPI
 import json
 import uvicorn
+from fastapi.responses import JSONResponse
+
+
 from insights.transcript import transcribe_video
 import modal
 app = FastAPI()
 
 @app.get("/insights/")
 def transcribe_url(url: str):
-    return json.dumps(transcribe_video(url))
+    return transcribe_video(url)
 
 @app.get("/marketSearch")
 def search_topic(prompt: str):
     run = modal.Function.lookup("market-research", "run")
     return json.dumps(run.remote(prompt))
+    # return "hi"
 
 if __name__ == "__main__":
     uvicorn.run("server:app", port=5000, log_level="info")
